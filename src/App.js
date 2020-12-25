88152eaa04b65ee51cb083514bc323f1e7cb1c34
Harish Dalal <harish-dalal@users.noreply.github.com>
2020-12-25 10:02:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Team from './components/Team';
 import Home from './components/Home';
 import Blog from './components/main';
 import Projects from './components/Projects';
+import Events from './components/Events';
+
 function App() {
   return(
     <div >
@@ -19,7 +21,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route exact path="/team" component={Team} />
       <Route exact path="/projects" component={Blog}/>
-      
+      <Route exact path="/events" component = {Events}/>
       </Switch>
       
     </div>
---
+++ src/components/Events.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const parallax = window.ParallaxCard
+
+function Events(){
+    // console.log(par)
+    return(
+        <div>
+            how world
+        </div>
+    )
+}
+
+export default Events
